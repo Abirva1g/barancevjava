2081521e6d3dbdf11208dbb21c9bf52a7ac849dd
Abirva1g <106960263+Abirva1g@users.noreply.github.com>
2024-01-14 22:02:18 +0300

    Update first.java

--- sandbox/src/main/java/first.java
+++ sandbox/src/main/java/first.java
@@ -2,5 +2,9 @@ public class first{
 
  public static void main (String[] args){
 System.out.println("Hello, world!");
+  System.out.println(2 + 2);
+  System.out.println(2 * 2);
+  System.out.println(2 / 2);
+  System.out.println(2 - 2);
 }
 }
